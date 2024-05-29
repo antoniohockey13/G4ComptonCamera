aaa2f9465f0618efb8de03aa8d8af1429f98da55
Jordi Duarte-Campderros <jorge.duarte.campderros@cern.ch>
2024-05-29 16:18:05 +0200

    Activate the em option 4 physics list

--- src/physics.cc
+++ src/physics.cc
@@ -1,11 +1,13 @@
 #include "physics.hh"
+#include "G4EmStandardPhysics_option4.hh"
 
 ComptCameraPhysicsList::ComptCameraPhysicsList()
 {
-	RegisterPhysics(new G4DecayPhysics());
-	RegisterPhysics(new G4EmLivermorePhysics());
-	RegisterPhysics(new G4EmStandardPhysics());
-	RegisterPhysics(new G4EmPenelopePhysics());	
+	//RegisterPhysics(new G4DecayPhysics());
+	//RegisterPhysics(new G4EmLivermorePhysics());
+	//RegisterPhysics(new G4EmStandardPhysics());
+	//RegisterPhysics(new G4EmPenelopePhysics());	
+	RegisterPhysics(new G4EmStandardPhysics_option4());	
 }
 ComptCameraPhysicsList::~ComptCameraPhysicsList()
 {
