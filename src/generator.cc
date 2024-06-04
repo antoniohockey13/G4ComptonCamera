5dcac3b37113fc5a857aeff63c7727108cf6fd81
antoniohockey13 <antoniogomezcarrera@gmail.com>
2024-06-04 13:00:10 +0200

    Added interpolation in GPS

--- src/generator.cc
+++ src/generator.cc
@@ -28,6 +28,8 @@ ComptCameraPrimaryGenerator::ComptCameraPrimaryGenerator():
     current_source->GetEneDist()->SetEnergyDisType("Arb");
 G4cout << "Current source: " << current_source << G4endl;
     current_source->GetEneDist()->ArbEnergyHistoFile("Source_sim_biased_109.txt"); 
+    current_source->GetEneDist()->ArbInterpolate("Lin");
+
 G4cout << "Personalised energy distribution" << G4endl;
 }
 
